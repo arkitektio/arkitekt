8824bcd37690eff857091dd6e1d4f0c630b1b9ce
jhnnsrs <jhnnsrs@gmail.com>
2022-02-21 18:18:07 +0100

    docs

--- arkitekt/arkitekt.py
+++ arkitekt/arkitekt.py
@@ -1,3 +1,49 @@
+"""The initialization Function for stuff
+
+This module demonstrates documentation as specified by the `Google
+Python Style Guide`_. Docstrings may extend over multiple lines.
+Sections are created with a section header and a colon followed by a
+block of indented text.
+
+Example:
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks:
+
+        ```python
+        print("Hello world")
+
+
+
+
+        ```
+
+
+
+        $ python example_google.py
+
+Section breaks are created by resuming unindented text. Section breaks
+are also implicitly created anytime a new section starts.
+
+Attributes:
+    module_level_variable1 (int): Module level variables may be documented in
+        either the ``Attributes`` section of the module docstring, or in an
+        inline docstring immediately following the variable.
+
+        Either form is acceptable, but the two should not be mixed. Choose
+        one convention to document module level variables and be consistent
+        with it.
+
+Todo:
+    * For module TODOs
+    * You have to also use ``sphinx.ext.todo`` extension
+
+.. _Google Python Style Guide:   
+http://google.github.io/styleguide/pyguide.html
+
+"""
+
+
 import os
 from arkitekt.errors import NoArkitektFound
 from rath import rath
@@ -12,6 +58,12 @@ logger = logging.getLogger(__name__)
 
 
 def set_current_arkitekt(herre, set_global=True):
+    """_summary_
+
+    Args:
+        herre (_type_): _description_
+        set_global (bool, optional): _description_. Defaults to True.
+    """
     global GLOBAL_ARKITEKT
     current_arkitekt.set(herre)
     if set_global:
@@ -53,4 +105,10 @@ def get_current_arkitekt(allow_global=True):
 
 
 class Arkitekt(rath.Rath):
+    """_summary_
+
+    Args:
+        rath (_type_): _description_
+    """
+
     pass
--- website/docs/reference/arkitekt.md
+++ website/docs/reference/arkitekt.md
@@ -3,3 +3,76 @@ sidebar_label: arkitekt
 title: arkitekt
 ---
 
+The initialization Function for stuff
+
+This module demonstrates documentation as specified by the `Google
+Python Style Guide`_. Docstrings may extend over multiple lines.
+Sections are created with a section header and a colon followed by a
+block of indented text.
+
+**Example**:
+
+  Examples can be given using either the ``Example`` or ``Examples``
+  sections. Sections support any reStructuredText formatting, including
+  literal blocks:
+  
+  ```python
+  print(&quot;Hello world&quot;)
+  
+  
+  
+  
+  ```
+  
+  
+  
+  $ python example_google.py
+  
+  Section breaks are created by resuming unindented text. Section breaks
+  are also implicitly created anytime a new section starts.
+  
+
+**Attributes**:
+
+- `module_level_variable1` _int_ - Module level variables may be documented in
+  either the ``Attributes`` section of the module docstring, or in an
+  inline docstring immediately following the variable.
+  
+  Either form is acceptable, but the two should not be mixed. Choose
+  one convention to document module level variables and be consistent
+  with it.
+  
+
+**Todo**:
+
+  * For module TODOs
+  * You have to also use ``sphinx.ext.todo`` extension
+  
+  .. _Google Python Style Guide:
+  http://google.github.io/styleguide/pyguide.html
+
+#### set\_current\_arkitekt
+
+```python
+def set_current_arkitekt(herre, set_global=True)
+```
+
+_summary_
+
+**Arguments**:
+
+- `herre` __type__ - _description_
+- `set_global` _bool, optional_ - _description_. Defaults to True.
+
+## Arkitekt Objects
+
+```python
+class Arkitekt(rath.Rath)
+```
+
+_summary_
+
+**Arguments**:
+
+- `rath` __type__ - _description_
+
